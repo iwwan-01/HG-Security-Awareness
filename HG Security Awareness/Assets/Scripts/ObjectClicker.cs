d1452f642aa39472ba03597bffd69abc7b9ffffd
pimat2 <daniel.batalski@abv.bg>
2022-06-06 20:20:16 +0200

    Animated the cabinets and imported Georgi's models

--- HG Security Awareness/Assets/Scripts/ObjectClicker.cs
+++ HG Security Awareness/Assets/Scripts/ObjectClicker.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class ObjectClicker : MonoBehaviour
 {
-    public Animator animator;
+    public Animator cameraAnimator;
+    public Animator cabinetAnimator;
+    public Animator secondCabinetAnimator;
+    public Animator lastCabinetAnimator;
     // Update is called once per frame
     void Update()
     {
@@ -18,11 +21,49 @@ public class ObjectClicker : MonoBehaviour
                 {
                     if(hit.collider.tag == "Laptop"){
                         Debug.Log("You clicked on laptop");
-                        animator.SetBool("laptopclick", true);
+                        cameraAnimator.SetBool("laptopclick", true);
+                    }
+                    if(hit.collider.tag == "Cabinet1"){
+                        Debug.Log("You clicked on the first cabinet");
+                        cabinetAnimator.SetBool("isclosed", true);
+                        cabinetAnimator.SetBool("isopened", false);
+                    }
+                    if(hit.collider.tag == "Cabinet2"){
+                        Debug.Log("You clicked on the second cabinet");
+                        secondCabinetAnimator.SetBool("isclosed", true);
+                        secondCabinetAnimator.SetBool("isopened", false);
+                    }
+                    if(hit.collider.tag == "Cabinet3"){
+                        Debug.Log("You clicked on the second cabinet");
+                        lastCabinetAnimator.SetBool("isclosed", true);
+                        lastCabinetAnimator.SetBool("isopened", false);
                     }
                 }
             }
         }
-    }
+        if(Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if(hit.collider != null)
+                {
+                    if(hit.collider.tag == "Cabinet1"){
+                        cabinetAnimator.SetBool("isopened", true);
+                        cabinetAnimator.SetBool("isclosed", false);
+                    }
+                    if(hit.collider.tag == "Cabinet2"){
+                        secondCabinetAnimator.SetBool("isopened", true);
+                        secondCabinetAnimator.SetBool("isclosed", false);
+                    }
+                    if(hit.collider.tag == "Cabinet3"){
+                        lastCabinetAnimator.SetBool("isopened", true);
+                        lastCabinetAnimator.SetBool("isclosed", false);
+                    }
+                }
+            }
     
+        }
+    }
 }
