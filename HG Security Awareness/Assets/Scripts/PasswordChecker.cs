bcead2b83239ae0e2447dc8e5a0ef4270e0efff5
pimat2 <daniel.batalski@abv.bg>
2022-06-14 20:14:34 +0200

    Made the Password Strength Meter
    CODE WISE AND MEMORY WISE IT"S UTTER SHIT BUT I CANT SEEM TO FIGURE ANOTHER WAY TO IMPLEMENT IT :)

--- HG Security Awareness/Assets/Scripts/PasswordChecker.cs
+++ HG Security Awareness/Assets/Scripts/PasswordChecker.cs
@@ -15,44 +15,79 @@ public class PasswordChecker : MonoBehaviour
     public Text symbolText;
     public GameObject slider;
     public Color color;
+    public Animator PasswordSliderAnimator;
+    public int Length = 0;
+    public int Digit = 0;
+    public int Letter = 0;
+    public int Uppercase = 0;
+    public int Symbol = 0;
+    public int PasswordStrength;
+    
 
     
      private void Update() {
         string PasswordString = passwordInputField.text;
+        PasswordStrength = Length + Digit + Letter + Uppercase + Symbol;
         if(PasswordString.Length > 8)
         {
             lengthText.color = Color.black;
+            Length = 1;
         }else
         {
             lengthText.color = Color.gray;
+            Length = 0;
         }
         if(PasswordString.Any(char.IsDigit))
         {
             digitText.color = Color.black;
+            Digit = 1;
         }else
         {
             digitText.color = Color.gray;
+            Digit = 0;
         }
         if(PasswordString.Any(char.IsLetter))
         {
             letterText.color = Color.black;
+            Letter = 1;
         }else
         {
             letterText.color = Color.gray;
+            Letter = 0;
         }
         if(PasswordString.Any(char.IsUpper))
         {
             uppercaseText.color = Color.black;
+            Uppercase = 1;
         }else
         {
             uppercaseText.color = Color.gray;
+            Uppercase = 0;
         }
         if(PasswordString.Any(char.IsSymbol) || PasswordString.Any(char.IsPunctuation))
         {
             symbolText.color = Color.black;
+            Symbol = 1;
         }else
         {
             symbolText.color = Color.gray;
+            Symbol = 0;
         }
+        if(PasswordStrength >= 2){
+            PasswordSliderAnimator.SetBool("PassWeak", true);
+        }else{
+            PasswordSliderAnimator.SetBool("PassWeak", false);
+        }
+        if(PasswordStrength >= 4){
+            PasswordSliderAnimator.SetBool("PassAverage", true);
+        }else{
+            PasswordSliderAnimator.SetBool("PassAverage", false);
+        }
+        if(PasswordStrength == 5){
+            PasswordSliderAnimator.SetBool("PassStrong", true);
+        }else{
+            PasswordSliderAnimator.SetBool("PassStrong", false);
+        }
+
 }
 }
